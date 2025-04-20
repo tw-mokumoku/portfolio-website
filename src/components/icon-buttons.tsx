4f4c847af90de4b7edf267861a01339de01f19b4
tw-mokumoku <takushi.watanabe.work@gmail.com>
2025-04-21 04:44:53 +0900

    tech icon buttonsの追加。homeコードのコンポーネント化

---
+++ src/components/icon-buttons.tsx
@@ -0,0 +1,31 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { FaDiscord, FaGithub, FaLinkedinIn } from "react-icons/fa";
+import { IoMail } from "react-icons/io5";
+
+export function TechIconButtons(){
+    return (
+        <div className="flex justify-center gap-8">
+        <Button variant="outline" size="icon">
+          <Link href="https://github.com/tw-mokumoku">
+            <FaGithub />
+          </Link>
+        </Button>
+        <Button variant="outline" size="icon">
+          <Link href="https://www.linkedin.com/in/%E6%B8%A1%E9%82%8A-%E7%90%A2%E8%B3%87-731b63327/">
+            <FaLinkedinIn />
+          </Link>
+        </Button>
+        <Button variant="outline" size="icon">
+          <Link href="https://discord.gg/BF6z7Dw9">
+            <FaDiscord />
+          </Link>
+        </Button>
+        <Button variant="outline" size="icon">
+          <Link href="mailto:takushi.watanabe.work@gmail.com">
+            <IoMail />
+          </Link>
+        </Button>
+      </div>
+    );
+}
