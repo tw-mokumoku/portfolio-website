4d3190b2f842c48ea397e40fc8925d95fdbfc224
admin-lingstudy <admin@lingstudy.com>
2025-04-21 01:10:35 +0900

    homeのTechLogoMarqueeをコンポーネント化しCSRにした

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,6 @@
-'use client'
-import Marquee from "react-fast-marquee";
-import Image from 'next/image'
-import { useWindowSize } from "react-use";
+import { TechLogoMarquee } from "@/components/marquee";
 
 export default function Home() {
-  const {width} = useWindowSize();
   return (
     <div className="home top-0">
       <div className="flex h-screen flex items-center justify-center">
@@ -29,70 +25,7 @@ export default function Home() {
               スクロールでスキル &amp; 経験を確認
             </p>
           </div>
-          <Marquee
-          speed={20}
-          autoFill={true}
-          gradient={true}
-          gradientWidth={width>=640?width>=1024?500:350:200}
-          gradientColor="#09090b"
-          >
-            <div className="h-15 mx-4">
-              <Image
-              src="/react_logo.svg"
-              alt="React Logo"
-              fill
-              style={{objectFit: 'contain'}}
-              />
-            </div>
-            <div className="h-22 mx-4">
-              <Image
-              src="/next_logo.svg"
-              alt="Next Logo"
-              fill
-              style={{objectFit: 'contain'}}
-              />
-            </div>
-            <div className="h-10 mx-4">
-              <Image
-              src="/git_logo.svg"
-              alt="Git Logo"
-              fill
-              style={{objectFit: 'contain'}}
-              />
-            </div>
-            <div className="h-23 mx-4">
-              <Image
-              src="/vercel_logo.svg"
-              alt="Vercel Logo"
-              fill
-              style={{objectFit: 'contain'}}
-              />
-            </div>
-            <div className="h-5 mx-4">
-              <Image
-              src="/tailwind_logo.svg"
-              alt="Tailwindcss Logo"
-              fill
-              style={{objectFit: 'contain'}}
-              />
-            </div>
-            <div className="h-13 mx-4">
-             <Image
-              src="/heroku_logo.svg"
-              alt="Heroku Logo"
-              fill
-              style={{objectFit: 'contain'}}
-              />
-            </div>
-            <div className="h-12 mx-4">
-             <Image
-              src="/neon_logo.png"
-              alt="Neon Logo"
-              fill
-              style={{objectFit: 'contain'}}
-              />
-            </div>
-          </Marquee>
+          <TechLogoMarquee />
         </div>
       </div>
     </div>
---
+++ src/components/marquee.tsx
@@ -0,0 +1,76 @@
+'use client'
+
+import Marquee from "react-fast-marquee";
+import Image from 'next/image'
+import { useWindowSize } from "react-use";
+
+export function TechLogoMarquee(){
+    const {width} = useWindowSize();
+
+    return (
+        <Marquee
+        speed={20}
+        autoFill={true}
+        gradient={true}
+        gradientWidth={width>=640?width>=1024?500:350:200}
+        gradientColor="#09090b"
+        >
+          <div className="h-15 mx-4">
+            <Image
+            src="/react_logo.svg"
+            alt="React Logo"
+            fill
+            style={{objectFit: 'contain'}}
+            />
+          </div>
+          <div className="h-22 mx-4">
+            <Image
+            src="/next_logo.svg"
+            alt="Next Logo"
+            fill
+            style={{objectFit: 'contain'}}
+            />
+          </div>
+          <div className="h-10 mx-4">
+            <Image
+            src="/git_logo.svg"
+            alt="Git Logo"
+            fill
+            style={{objectFit: 'contain'}}
+            />
+          </div>
+          <div className="h-23 mx-4">
+            <Image
+            src="/vercel_logo.svg"
+            alt="Vercel Logo"
+            fill
+            style={{objectFit: 'contain'}}
+            />
+          </div>
+          <div className="h-5 mx-4">
+            <Image
+            src="/tailwind_logo.svg"
+            alt="Tailwindcss Logo"
+            fill
+            style={{objectFit: 'contain'}}
+            />
+          </div>
+          <div className="h-13 mx-4">
+           <Image
+            src="/heroku_logo.svg"
+            alt="Heroku Logo"
+            fill
+            style={{objectFit: 'contain'}}
+            />
+          </div>
+          <div className="h-12 mx-4">
+           <Image
+            src="/neon_logo.png"
+            alt="Neon Logo"
+            fill
+            style={{objectFit: 'contain'}}
+            />
+          </div>
+        </Marquee>
+    );
+}
