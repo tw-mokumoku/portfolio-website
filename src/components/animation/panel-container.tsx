6840feedf0fcd41de9449efee8b3a40cbfc19e05
tw-mokumoku <takushi.watanabe.work@gmail.com>
2025-04-25 08:45:21 +0900

    プロフィールパネルの作成完了

--- src/components/animation/panel-container.tsx
+++ src/components/animation/panel-container.tsx
@@ -18,13 +18,13 @@ export function TransparentContainer(
     const rotateX = useTransform(
         mouseYSpring,
         [0.5, -0.5],
-        ["5deg", "-5deg"]
+        ["0.5deg", "-0.5deg"]
     );
     
     const rotateY = useTransform(
         mouseXSpring, // 水平方向の動きを使用
         [0.5, -0.5],
-        ["-5deg", "5deg"] // 符号を修正
+        ["-0.5deg", "0.5deg"] // 符号を修正
     );
     
     
@@ -49,55 +49,57 @@ export function TransparentContainer(
         y.set(0);
     };
 
-    const iconVariants = {
-        whileSkillIconHover: { scale: 1.2 },
-        whileSkillIconTap: { scale: 0.95 }
+    const transparentFilter = {
+        whileSkillIconHover: { opacity: 0 },
     }
 
     const frameVariants = {
-        whileSkillIconHover: { scale: 1.1 },
-        whileSkillIconTap: { scale: 0.95 }
+        whileSkillIconHover: { scale: 1 },
+        whileSkillIconTap: { scale: 0.9 }
     }
 
     const frameOpacity = {
-        whileSkillIconHover: { opacity: 0.1 },
+        whileSkillIconHover: { opacity: 0.2 },
         whileSkillIconTap: { opacity: 0.2 }
     }
 
     return (
         <motion.button
-        className="h-25 2xl:h-40 w-40 2xl:w-64"
+        className="w-200"
         whileHover = "whileSkillIconHover"
         onMouseMove={handleMouseMove}
         onMouseLeave={handleMouseLeave} // マウスリーブ時の処理を追加
-        style={{
-            transformStyle: "preserve-3d",
-            perspective: 600 // 遠近感を追加
-        }}
         whileTap = "whileSkillIconTap"
         >
             <motion.div
-            className="h-full flex flex-col items-center justify-center gap-1 outline rounded-xl hover:outline-white hover:outline-2"
+            className="h-full flex flex-col items-center justify-center gap-1 rounded-xl p-10 hover:outline-2 hover:outline-white"
             variants={frameVariants}
             style={{
-                rotateX,
-                rotateY,
-                transformStyle: "preserve-3d"
+                scale: 0.7
             }}
             >
                 <motion.div
                 style={{
-                    background: "linear-gradient(45deg, #3a3c47 0%, #6b7077 100%)", opacity: 0
+                    background: "linear-gradient(45deg, #3a3c47 0%, #6b7077 100%)", opacity: 0.1,
                 }}
-                className="h-full w-full absolute rounded-xl"
+                className="h-full w-full absolute rounded-xl z-1"
                 variants={frameOpacity}
                 />
-                <motion.div
-                variants={iconVariants}
-                className="h-full"
+                <motion.div className="h-full w-full absolute rounded-xl z-3 flex items-center justify-center"
                 style={{
-                    transformStyle: "preserve-3d"
+                    background: "linear-gradient(45deg, #3a3c47 0%, #6b7077 100%)",
+                    opacity: 0.75,
                 }}
+                variants={transparentFilter}
+                >
+                    <p className="text-9xl z-4 text-gray-900 font-bold">Hover</p>
+                </motion.div>
+                <motion.div
+                style={{
+                    scale: 1,
+                    
+                }}
+                className="z-2"
                 >
                     {children}
                 </motion.div>
